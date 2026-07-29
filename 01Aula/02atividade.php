128900d5cb6eebf62b24e140e92bb7fc6aa678f0
ANA BARBARA AQUINO BARBOSA <ana.barbosa205@portalsesisp.org.br>
2026-07-29 13:19:19 -0300

    primeiro commit

---
+++ 01Aula/02atividade.php
@@ -0,0 +1,16 @@
+<?php  
+    // Declaração de variavel
+    $nome = "Estudante";
+    $idade = 20;
+
+    // Exibindo informaçaoes
+    echo "Olá,  $nome!";
+    echo "Você tem $idade anos.";
+
+    // Condicional simples
+    if($idade >= 18){
+        echo "Voce é maior de idade";
+    }else{
+        echo "Voce é menor de idade";
+    }
+?>
